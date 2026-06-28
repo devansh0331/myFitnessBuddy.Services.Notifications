3f4ac7a87c4ecb6ab82ad9c553f48840fe22ea3c
devansh0331 <devanshshrivastava07@gmail.com>
2026-06-28 14:33:40 +0530

    service account credentials added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,11 @@
 const admin = require('firebase-admin');
 
-function initializeFirebase() {
-  if (!admin.apps.length) {
-    // If the secret is stored as an env variable, parse it
-    const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-      ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-      : require('../../service-account.json');
+// Reads the file that GitHub Actions created
+const serviceAccount = require('../../service-account.json');
 
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      projectId: serviceAccount.project_id,
-    });
-  }
-  return admin;
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  projectId: serviceAccount.project_id,
+});
 
-module.exports = { initializeFirebase, admin };
+module.exports = { admin };
